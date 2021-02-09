e6f3fe2131f88ee9766fa47104b290d5c3ec43bd
ItzFaded <33379986+ItzFaded@users.noreply.github.com>
2021-02-09 22:08:56 +0100

    aaaaa

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EKSTRA: Brukerstøtteskjema HTML</title>
+</head>
+<body>
+    <form action="/submit.html" method="post"></form>
+        <div id="1">
+            <p>Hvordan synes du det er å arbeide her</p>
+            <label for="1">1: Dårlig</label>
+            <label for="3">3: Passe</label>
+            <label for="5">5: Supert</label><br><br>
+            <input type="radio" id="1">
+            <input type="radio" id="2">
+            <input type="radio" id="3">
+            <input type="radio" id="4">
+            <input type="radio" id="5"><br><br>
+            <input type="submit" value="Submit">
+        </div>
+
+</body>
+</html>
